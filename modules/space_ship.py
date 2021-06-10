f279527cf0ddc34c2e5218921959ab295db43779
Rodrigo Trindade <roelisontrindade@outlook.com>
2021-06-10 19:20:37 -0300

    feat: :sparkles: Add Spaceship modules

---
+++ modules/space_ship.py
@@ -0,0 +1,10 @@
+import turtle
+
+#----------Space-Ship----------#
+ship = turtle.Turtle()
+ship.speed(0)
+ship.color("blue")
+ship.shape("triangle")
+ship.penup()
+ship.setposition(0, -250)
+ship.setheading(90)
