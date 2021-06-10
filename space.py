6e0871fab3be0530afe35f4814b509538af5255d
Rodrigo Trindade <roelisontrindade@outlook.com>
2021-06-10 19:22:13 -0300

    feat: :sparkles: Import spaceship module
    Add main loop e call ship element from spaceship module and draw a screen border

--- space.py
+++ space.py
@@ -7,24 +7,41 @@ import turtle
 
 #----------Modules-----------------#
 
+import modules.space_ship as ship
 
 #----------window setup------------#
 
 window = turtle.Screen()
-window.title("Classic Pong")
+window.title("Classic Space Invaders")
 window.bgcolor("Black")
-window.setup(width=800, height=600)
+window.setup(width=700, height=700)
 window.tracer(0, 0)
 
+#----------Screen-Border-----------#
+
+border = turtle.Turtle()
+border.speed(0)
+border.color("white")
+border.penup()
+border.setposition(-300, -300)
+border.pendown()
+border.pensize(3)
+
+for side in range(4):
+    border.fd(600)
+    border.lt(90)
+
+border.hideturtle()
 
 #----------window changes----------#
 
 
 
-
 #----------Maingame loop-----------#
 def main():
-    pass
+    while True:
+        window.update()
+        ship
         
 if __name__ == "__main__":
     main()
