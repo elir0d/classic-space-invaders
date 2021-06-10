c586b3e3e8cd6d5e9500b61973686d5433d89ce5
Rodrigo Trindade <roelisontrindade@outlook.com>
2021-06-10 00:41:39 -0300

    feat: :tada: Add main game python file

---
+++ space.py
@@ -0,0 +1,30 @@
+#----------Library-----------------#
+
+import os
+import sys
+import time
+import turtle
+
+#----------Modules-----------------#
+
+
+#----------window setup------------#
+
+window = turtle.Screen()
+window.title("Classic Pong")
+window.bgcolor("Black")
+window.setup(width=800, height=600)
+window.tracer(0, 0)
+
+
+#----------window changes----------#
+
+
+
+
+#----------Maingame loop-----------#
+def main():
+    pass
+        
+if __name__ == "__main__":
+    main()
