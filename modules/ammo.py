5d8d78cda893093d65b30e87b3b7375fa3072c9e
Rodrigo Trindade <roelisontrindade@outlook.com>
2021-06-27 21:37:09 -0300

    feat: :sparkles: Create a ammo module
    This module include de "bullet" element

---
+++ modules/ammo.py
@@ -0,0 +1,15 @@
+import turtle
+
+#-----bullet-element-----#
+
+bullet = turtle.Turtle()
+bullet.color("yellow")
+bullet.shape("triangle")
+bullet.penup()
+bullet.speed(0)
+bullet.setheading(90)
+bullet.shapesize(0.3, 0.3)
+bullet.hideturtle()
+bulletspeed = 20
+
+#-----bullet-element-----#
